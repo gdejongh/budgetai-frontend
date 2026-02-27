7726fecd6faf8bddea14f727175912f3a2e6308b
Gabe <dejonghgabe1@gmail.com>
2026-02-27 15:52:04 -0500

    fix: update API URL to use secure HTTPS endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Note: We are using HTTP for now until your custom domain SSL is ready
-  apiUrl: 'http://budgetai-backend-env.eba-kfntrwtp.us-east-1.elasticbeanstalk.com'
+  apiUrl: 'https://api.aienvelopebudget.com'
 };
